bfc0c57da3139a58767276fad45daf766e24117d
Great Nnaji <greatnnaji@cmail.carleton.ca>
2026-04-08 19:16:17 -0400

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # idea water held at some i is determined by min between max height (left)
+        # at max height (right)
+
+        max_hl = [0]
+        max_hr = [0]
+
+        for h in height:
+            if h > max_hl[-1]:
+                max_hl.append(h)
+            else:
+                max_hl.append(max_hl[-1])
+        
+        max_hl = max_hl[:len(height)]
+        # print(max_hl)
+
+        for h in reversed(height):
+            if h > max_hr[-1]:
+                max_hr.append(h)
+            else:
+                max_hr.append(max_hr[-1])
+        
+        max_hr = list(reversed(max_hr))
+        max_hr = max_hr[1:]
+        # print(max_hr)
+
+        min_list = []
+        for i, h in enumerate(height):
+            min_list.append(min(max_hl[i], max_hr[i]))
+
+        print(min_list)
+        total = 0
+        for i, v in enumerate(min_list):
+            calc = v - height[i]
+            if calc > 0:
+                total += calc
+        
+        print(total)
+
+
+
+        
+        return total
